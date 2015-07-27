99b46ed67e9efcddb18b0356923bfdb2c6e6c9de
zyking <erkalmar@gmail.com>
2015-07-27 11:30:19 -0500

    Create directives with elements and class

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,24 +1,37 @@
+
+
 	var app = angular.module("Vinco",[]);
 
-//Creamos un nuevo controlador llamado "Blog" , Declaramos $http y $scope
-		app.controller("Orbis",function($scope,$http){
-//Declaramos una variable llamada post , donse se almacenaran 
-			$scope.imgs = [];
-//Realizamos un llamado a la API Rest JSON de Jsonplaceholder
-			$http.get("http://jsonplaceholder.typicode.com/photos")
-//Declaramos que si fue exitosa la accion ejecute la funcion
-			.success(function(data){
-				//Ejecutamos la funcion , la cual ejecutaremos en la vista para mostrar los posts.
-						$scope.imgs = data;
-						console.log(data);
-				});
-				});
-
-//Creamos la directiva
-		app.directive('vincoOrbis', function() {
-			    return {
-			     restrict : 'E',
-			     templateUrl : './templates/template.html',
-			    // templateUrl : "./templates/template.html",
-			    }
+	app.directive("vincoOrbis", function(){
+		 return {
+//Declaramos que sera un elemento 		 	
+		      restrict: 'E',
+//Contenido de la directiva
+		  	  template : "<h1>Hey Listo!!</h1>"
+		    }
 			});
+
+
+		app.directive("vincoDirective", function(){
+		 return {
+//Declaramos que sera un elemento 		 	
+		      restrict: 'C',
+//Contenido de la directiva
+		  	  template : "<h1>{{test}}</h1>"
+		    }
+			});
+
+		app.directive("vincotemplate", function(){
+		 return {
+//Declaramos que sera un elemento 		 	
+		      restrict: 'E',
+//Contenido de la directiva
+		  	  template : "<h1>{{test}}</h1>"
+		    }
+			});
+
+	app.controller("Orbis",function($scope){
+
+		$scope.test = "test";
+
+	});
